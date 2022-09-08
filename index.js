9f10ffa277586b375e0e6bfa0477cf3a93ccb55b
RocioRD <rociomariel21@gmail.com>
2022-09-08 17:35:04 -0300

    actualizacion hbs, corrección paths, se agrego nomemon

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+require('dotenv').config();
+const path = require('path');
+const hbs = require('hbs');
+const  mysql = require('mysql2');
+const nodemailer = require('nodemailer');
+const { resourceLimits } = require('worker_threads');
+const app = express();
+const PORT = 8080;
+
+// configurar middelwares
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// configuracion del motor de plantillas
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+hbs.registerPartials(path.join(__dirname, 'views/partials'))
+//connection.end();
+
+
+app.listen(PORT, () => {
+  // console.log(`el servidor esta trabajando en el Puerto ${PORT}`);
+ });
+ 
+app.get('/', (req, res) => {
+res.render('index', {
+  titulo: 'bienvenidos a la app de la utn',
+})
+});
