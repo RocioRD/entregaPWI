943696c758e273e6d72793d64f972b653a7b8b4c
RocioRD <rociomariel21@gmail.com>
2022-09-11 16:01:09 -0300

    actualizacion sql y suscripcion hsb

--- entregaPWI.sql
+++ entregaPWI.sql
@@ -1,14 +1,12 @@
 create database entregaPwi;
 use entregaPwi;
-use suscripcion;
-
 
 -- creacion de la tabla suscripcion --
-create table suscripcion(
-suscriptorId int unsigned not null auto_increment primary key,
-nombre varchar(50) not null,
+create table user(
+userId int unsigned not null auto_increment primary key,
+userName varchar(50) not null,
 email varchar(100) not null,
-edad smallint
+password varchar(100) not null
 );
 
 
